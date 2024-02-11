4d1dad6214b89ad557fd32f85d2e8b802ca40695
Shivamzee <hello.shivamzee@gmail.com>
2024-02-11 19:05:23 +0530

    created todo en in js ,some work localeStorage

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavaScript-project-Responsive-ToDo-App
-This is a responsive todo app, it has the feature to tell you the status of your daily tasks . It uses localStorage to store your data
+
+<b> This is a responsive todo app, it has the feature to tell you the stats of your daily tasks . It uses localStorage to store your data.a</b>
--- todo.js
+++ todo.js
@@ -1,4 +1,76 @@
 console.log("This app is working , code by shivam sharma");
-if ("shivam") {
-  console.log("yes sir");
+
+// selecting elements from html file
+
+const getToDoform = document.querySelector("#todo-form");
+const todoList = document.querySelector(".todos");
+const remainingTasksbtn = document.querySelector("#remaining-tasks");
+const completedTasksbtn = document.querySelector("#completed-tasks");
+const totalTasksbtn = document.querySelector("#total-tasks");
+
+// targeting input box
+
+const mainInputBox = document.querySelector("#todo-form input");
+
+let tasksList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+getToDoform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let inputValue = mainInputBox.value;
+  console.log(inputValue);
+
+  if (inputValue === " ") {
+    return;
+  }
+
+  const task = {
+    id: new Date().getTime(),
+    tName: inputValue,
+    isCompleted: false,
+  };
+
+  // console.log(task);
+  // console.log(task.name);
+
+  tasksList.push(task);
+  localStorage.setItem("tasksList", JSON.stringify(tasksList));
+
+  mainInputBox.focus();
+  getToDoform.reset();
+
+  cretetasks(task);
+});
+
+function createtasks(task) {
+  const taskEl_li = document.createElement("li");
+  taskEl_li.setAttribute("id", task.id);
+
+  if (task.isCompleted) {
+    taskEl_li.classList.add("completed");
+  }
+
+  const taskEl_html = `
+      <div>
+      <input type="checkbox" name="tasks" id=" ${task.id} " ${
+    task.isCompleted ? "chacked" : " "
+  } />
+      <span  ${!task.isCompleted ? "contenteditable" : " "} >${
+    task.tName
+  }</span>
+      </div>
+
+      <button title="Remove the" ${task.tName}  class="remove-task">
+      <i class="uil uil-multiply"></i>
+      </button>
+
+
+  `;
+
+  // console.log(taskEl_html);
+  taskEl_li.innerHTML = taskEl_html;
+
+  console.log(taskEl_li);
+
+  todoList.appendChild(taskEl_li);
 }
